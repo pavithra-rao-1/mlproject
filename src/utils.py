c1119b50deb0a85cc912e0f10291c446743035e9
pavithra-rao-1 <pavithrarao2003@gmail.com>
2025-06-16 22:03:14 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,9 @@ import sys
 import os
 import dill
 
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+    
 from src.exception import CustomException
 from src.logger import logging
 
@@ -15,3 +18,34 @@ def save_object(file_path, obj):
     except Exception as e:
         logging.error(f"Error occurred while saving object: {e}")
         raise CustomException(e, sys) from e
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    
+    model_report = {}
+    
+    for model_name, model in models.items():
+        try:
+            report = {}
+
+            for i in range(len(list(models))):
+                model = list(models.values())[i]
+                para = params[list(models.keys())[i]]
+
+                gs = GridSearchCV(model,para,cv=3)
+                gs.fit(X_train,y_train)
+
+                model.set_params(**gs.best_params_)
+                model.fit(X_train,y_train)
+
+                y_train_pred = model.predict(X_train)
+                y_test_pred = model.predict(X_test)
+
+                train_model_score = r2_score(y_train, y_train_pred)
+                test_model_score = r2_score(y_test, y_test_pred)
+
+                report[list(models.keys())[i]] = test_model_score
+
+            return report
+
+        except Exception as e:
+            raise CustomException(e, sys)
